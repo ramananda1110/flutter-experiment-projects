1fbfdc2c46910088db3d7ae1855b5fe9fcd31027
Ramananda Sarkar <rshc4u@gmail.com>
2020-09-12 22:43:20 +0600

    init shop app

---
+++ my_shop/android/app/src/main/kotlin/com/test/my_shop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.test.my_shop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ my_shop/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyShop'),
+      ),
+      body: Center(
+        child: Text('Let\'s go my shop app!'),
+      ),
+    );
+  }
+}
