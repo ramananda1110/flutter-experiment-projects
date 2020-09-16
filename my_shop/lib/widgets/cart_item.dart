e76ceca5f2193ea07fa4768a6a3e367cd4c5108a
Ramananda Sarkar <rshc4u@gmail.com>
2020-09-16 23:05:32 +0600

    show alert dialog to the reomve cart item

--- my_shop/lib/widgets/cart_item.dart
+++ my_shop/lib/widgets/cart_item.dart
@@ -28,6 +28,27 @@ class CartItem extends StatelessWidget {
         margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
       ),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+        return showDialog(
+            context: context,
+            builder: (ctx) => AlertDialog(
+                  title: Text('Are you sure?'),
+                  content:
+                      Text('Do you want to remove the item from the cart?'),
+                  actions: [
+                    FlatButton(
+                        onPressed: () => {
+                              Navigator.of(context).pop(false),
+                            },
+                        child: Text('No')),
+                    FlatButton(
+                        onPressed: () => {
+                              Navigator.of(context).pop(true),
+                            },
+                        child: Text('Yes')),
+                  ],
+                ));
+      },
       onDismissed: (direction) {
         // need to function
         Provider.of<Cart>(context, listen: false).removeItem(productId);
