3cca6cf0a554c65660a17ecea7145794c55381fd
Ramananda Sarkar <rshc4u@gmail.com>
2020-09-16 13:11:27 +0600

    adding product details data

--- my_shop/lib/screens/product_details_screen.dart
+++ my_shop/lib/screens/product_details_screen.dart
@@ -14,9 +14,6 @@ class ProductDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
 
-    final productData = Provider.of<Products>(context);
-    //final products = productData.findById(productId);
-
     final loadedProduct = Provider.of<Products>(
       context,
       listen: false,
@@ -25,6 +22,37 @@ class ProductDetailsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(loadedProduct.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              '৳${loadedProduct.price}',
+              style: TextStyle(fontSize: 20, color: Colors.grey),
+            ),
+
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                loadedProduct.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+                style: TextStyle(fontSize: 20, color: Colors.grey),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
