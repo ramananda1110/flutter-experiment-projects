b703ce84993f02884b9dc9e82d8d22ca71996435
ramananda1110 <rshc4u@gmail.com>
2020-12-18 23:25:33 +0600

    finished authtication & logout

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:chat_app/screen/auth_screen.dart';
+import 'package:chat_app/screen/chat_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -13,21 +15,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FlutterChat',
-      theme: ThemeData(
-          primarySwatch: Colors.pink,
-          backgroundColor: Colors.pink,
-          accentColor: Colors.deepPurple,
-          accentColorBrightness: Brightness.dark,
-          cardTheme: CardTheme.of(context).copyWith(
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12))),
-          buttonTheme: ButtonTheme.of(context).copyWith(
-              buttonColor: Colors.pink,
-              textTheme: ButtonTextTheme.primary,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20)))),
-      home: AuthScreen(),
-    );
+        title: 'FlutterChat',
+        theme: ThemeData(
+            primarySwatch: Colors.pink,
+            backgroundColor: Colors.pink,
+            accentColor: Colors.deepPurple,
+            accentColorBrightness: Brightness.dark,
+            cardTheme: CardTheme.of(context).copyWith(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12))),
+            buttonTheme: ButtonTheme.of(context).copyWith(
+                buttonColor: Colors.pink,
+                textTheme: ButtonTextTheme.primary,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)))),
+        home: StreamBuilder(
+            stream: FirebaseAuth.instance.onAuthStateChanged,
+            builder: (ctx, userSnapshot) {
+              if (userSnapshot.hasData) {
+                return ChatScreen();
+              }
+
+              return AuthScreen();
+            }));
   }
 }
--- chat_app/lib/screen/chat_screen.dart
+++ chat_app/lib/screen/chat_screen.dart
@@ -1,43 +1,58 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
-  /*Future<void> initializeDefault() async {
-    FirebaseApp app = await Firebase.initializeApp();
-    assert(app != null);
-    print('Initialized default app $app');
-  }*/
-
   @override
   Widget build(BuildContext context) {
     // await Firebase.initializeApp();
     return Scaffold(
         appBar: AppBar(
-          title: Text('list view'),
+          title: Text('FlutterChat'),
+          actions: [
+            DropdownButton(
+              icon: Icon(
+                Icons.more_vert,
+                color: Theme.of(context).primaryIconTheme.color,
+              ),
+              items: [
+                DropdownMenuItem(
+                  child: Container(
+                    child: Row(
+                      children: [
+                        Icon(Icons.exit_to_app),
+                        SizedBox(width: 8,),
+                        Text('Logout')
+                      ],
+                    ),
+                  ),
+                  value: 'logout' ,
+                )
+              ],
+              onChanged: (itemIdentifier) {
+                if (itemIdentifier == 'logout') {
+                  FirebaseAuth.instance.signOut();
+                }
+              },
+            )
+          ],
         ),
         body: StreamBuilder(
           stream: Firestore.instance
               .collection('chats/nNDXE8rd3c7eXfz1gks1/messages/')
               .snapshots(),
-
-
-
-         builder: (ctx, streamSnapshot)
-
-          {
-
-
+          builder: (ctx, streamSnapshot) {
             return ListView.builder(
                 itemCount: 10,
-                itemBuilder: (ctx, index)=>Container(
-                padding: EdgeInsets.all(10),
-                  child: Text('is work'),
-            ));
+                itemBuilder: (ctx, index) => Container(
+                      padding: EdgeInsets.all(10),
+                      child: Text('is work'),
+                    ));
 
             //*if (streamSnapshot.connectionState == ConnectionState.waiting) {
-               return Center(
-                child: CircularProgressIndicator(),
-              );
+            return Center(
+              child: CircularProgressIndicator(),
+            );
             //*
             //final documents = streamSnapshot.data.toString();
           },
