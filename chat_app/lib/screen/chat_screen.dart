4904d7815d0baa306decc532d843db590e0a7865
ramananda1110 <rshc4u@gmail.com>
2020-12-19 23:37:28 +0600

    listening the chat message

--- chat_app/lib/screen/chat_screen.dart
+++ chat_app/lib/screen/chat_screen.dart
@@ -1,61 +1,48 @@
+import 'package:chat_app/widget/chat/messages.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // await Firebase.initializeApp();
     return Scaffold(
-        appBar: AppBar(
-          title: Text('FlutterChat'),
-          actions: [
-            DropdownButton(
-              icon: Icon(
-                Icons.more_vert,
-                color: Theme.of(context).primaryIconTheme.color,
-              ),
-              items: [
-                DropdownMenuItem(
-                  child: Container(
-                    child: Row(
-                      children: [
-                        Icon(Icons.exit_to_app),
-                        SizedBox(width: 8,),
-                        Text('Logout')
-                      ],
-                    ),
+      appBar: AppBar(
+        title: Text('FlutterChat'),
+        actions: [
+          DropdownButton(
+            icon: Icon(
+              Icons.more_vert,
+              color: Theme.of(context).primaryIconTheme.color,
+            ),
+            items: [
+              DropdownMenuItem(
+                child: Container(
+                  child: Row(
+                    children: [
+                      Icon(Icons.exit_to_app),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text('Logout')
+                    ],
                   ),
-                  value: 'logout' ,
-                )
-              ],
-              onChanged: (itemIdentifier) {
-                if (itemIdentifier == 'logout') {
-                  FirebaseAuth.instance.signOut();
-                }
-              },
-            )
-          ],
+                ),
+                value: 'logout',
+              )
+            ],
+            onChanged: (itemIdentifier) {
+              if (itemIdentifier == 'logout') {
+                FirebaseAuth.instance.signOut();
+              }
+            },
+          )
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: [Expanded(child: Messages())],
         ),
-        body: StreamBuilder(
-          stream: Firestore.instance
-              .collection('chats/nNDXE8rd3c7eXfz1gks1/messages/')
-              .snapshots(),
-          builder: (ctx, streamSnapshot) {
-            return ListView.builder(
-                itemCount: 10,
-                itemBuilder: (ctx, index) => Container(
-                      padding: EdgeInsets.all(10),
-                      child: Text('is work'),
-                    ));
-
-            //*if (streamSnapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-            //*
-            //final documents = streamSnapshot.data.toString();
-          },
-        ));
+      ),
+    );
   }
 }
---
+++ chat_app/lib/widget/chat/messages.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class Messages extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('chat').snapshots(),
+      builder: (ctx,  chatSnapshot) {
+        if (chatSnapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        final chatDocs = chatSnapshot.data.documents;
+
+        return ListView.builder(
+            itemCount: chatDocs.length,
+            itemBuilder: (ctx, index) => Text(chatDocs[index]['text']));
+      },
+    );
+  }
+}
