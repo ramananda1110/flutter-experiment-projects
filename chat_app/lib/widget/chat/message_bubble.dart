37065903d3fe5076ce481a08a653f5b422f74158
ramananda1110 <rshc4u@gmail.com>
2020-12-21 00:42:17 +0600

    fixed createdid issues

--- chat_app/lib/widget/chat/message_bubble.dart
+++ chat_app/lib/widget/chat/message_bubble.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 class MessageBubble extends StatelessWidget {
   final String message;
   final bool isMe;
-
-  MessageBubble(this.message, this.isMe);
+  final Key key;
+  MessageBubble(this.message, this.isMe, {this.key});
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +21,7 @@ class MessageBubble extends StatelessWidget {
                 bottomLeft: !isMe ? Radius.circular(0) : Radius.circular(12),
                 bottomRight: isMe ? Radius.circular(0) : Radius.circular(12),
               )),
-          width: 140,
+          width: 200,
           padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
           margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
           child: Text(
